e44338f4cab17463872d5590a27ffa93202be0e1
Mark Ryall <mark@ryall.name>
2012-02-26 16:28:02 +1000

    added a simple binding to provide direct access to browser

--- lib/ripl-watir.rb
+++ lib/ripl-watir.rb
@@ -1,7 +1,25 @@
-require "ripl-watir/version"
+require 'ripl'
+require 'watir-webdriver'
 
 module Ripl
   module Watir
-    # Your code goes here...
+    @browser = ::Watir::Browser.new
+    class << self
+      attr_reader :browser
+    end
+
+    module Commands
+      def on_page *args
+        puts "creating page #{args.inspect}"
+      end
+
+      def visit_page *args
+        puts "visiting #{args.inspect}"
+      end
+    end
   end
 end
+
+Ripl::Commands.include Ripl::Watir::Commands
+browser = Ripl::Watir.browser
+Ripl.start :binding => binding
