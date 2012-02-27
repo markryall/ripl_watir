a94a5a929b7fb3c63525226c61f5e9a267b6d636
Mark Ryall <mark@ryall.name>
2012-02-27 11:54:47 +1000

    switched to forwardable for more effective tab completion on page methods

--- lib/ripl_watir/page.rb
+++ lib/ripl_watir/page.rb
@@ -1,10 +1,14 @@
-require 'delegate'
+require 'forwardable'
 
 module RiplWatir
-  class Page < DelegateClass Watir::Browser
+  class Page
+    attr_reader :browser
+    extend Forwardable
+
+    def_delegators :@browser, :title, :url, :html, :status, :refresh, :back
+
     def initialize browser
       @browser = browser
-      super browser
     end
   end
 end
